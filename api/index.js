a60e3ade04ee4434c4260f51009c9a62fa8990be
anton <antonjollant@aol.com>
2024-05-15 17:29:32 +0200

    +

--- api/index.js
+++ api/index.js
@@ -144,7 +144,7 @@ const DB = [
 ];
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "index.html");
 });
 
 app.get("/faq", (req, res) => {
